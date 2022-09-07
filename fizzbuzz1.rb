2392e10a8fc0645e3e534c43beaa13d877c4d27b
ZakCannon <Zak.Cannon@softwire.com>
2022-09-07 12:14:33 +0100

    added prompt for which rules

--- fizzbuzz1.rb
+++ fizzbuzz1.rb
@@ -1,37 +1,43 @@
+puts "Rule selection. as per softwire docs. Type numbers (out of 3, 5, 7, 11, 13, and 17) for which you want the standard
+rules to apply, separated by a space"
+rules = gets.chomp
+rules.split(/ /)
+
 puts "Enter range: from"
 first_n = gets.chomp.to_i
 puts "To"
 last_n = gets.chomp.to_i
+puts "Result: \n"
 
 for n in first_n..last_n do
   printstring = []
-  if n % 3 == 0
+  if n % 3 == 0 && rules.include?("3")
     printstring.push("Fizz")
   end
 
-  if n % 13 == 0
+  if n % 13 == 0 && rules.include?("13")
     printstring.push("Fezz")
   end
 
-  if n % 5 == 0
+  if n % 5 == 0 && rules.include?("5")
     printstring.push("Buzz")
   end
 
-  if n % 7 == 0
+  if n % 7 == 0 && rules.include?("7")
     printstring.push("Bang")
   end
 
-  if n % 11 == 0
+  if n % 11 == 0 && rules.include?("11")
     printstring = ["Bong"]
   end
 
-  if n % 13 == 0
+  if n % 13 == 0 && rules.include?("13")
     if !printstring.include?("Fezz")
       printstring.unshift("Fezz")
     end
   end
 
-  if n % 17 == 0
+  if n % 17 == 0 && rules.include?("17")
     printstring.reverse!
   end
 
