e1a5cdb042496388f6bb85beef56451a26c525d8
ZakCannon <Zak.Cannon@softwire.com>
2022-09-07 11:55:39 +0100

    finished part two using arrays

--- fizzbuzz1.rb
+++ fizzbuzz1.rb
@@ -1,27 +1,41 @@
-for n in 1..200 do
-  printstring = ""
+for n in 1..300 do
+  printstring = []
   if n % 3 == 0
-    printstring = printstring + "Fizz"
+    printstring.push("Fizz")
+  end
+
+  if n % 13 == 0
+    printstring.push("Fezz")
   end
 
   if n % 5 == 0
-    printstring = printstring + "Buzz"
+    printstring.push("Buzz")
   end
 
   if n % 7 == 0
-    printstring = printstring + "Bang"
+    printstring.push("Bang")
   end
 
   if n % 11 == 0
-    printstring = "Bang"
+    printstring = ["Bong"]
   end
 
-  if printstring == ""
+  if n % 13 == 0
+    if !printstring.include?("Fezz")
+      printstring.unshift("Fezz")
+    end
+  end
+
+  if n % 17 == 0
+    printstring.reverse!
+  end
+
+  if printstring == []
     printstring = false
   end
 
   if printstring
-    puts printstring
+    puts printstring.join()
   else
     puts n
   end
